23e6d1d0196e1bb13406ec07120722ef6268055c
Diwakar Sharma <diwakarsharma95@gmail.com>
2020-05-24 10:00:51 +1000

    Arrays in JSX

--- src/index.js
+++ src/index.js
@@ -25,6 +25,7 @@ const removeAll = () => {
 	render();
 };
 const appRoot = document.getElementById('root');
+
 const render = () => {
 	const template = (
 		<div>
@@ -33,9 +34,11 @@ const render = () => {
 			<p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
 			<p>{app.options.length}</p>
 			<button onClick={removeAll}>Remove All</button>
+
 			<ol>
-				<li>Item one</li>
-				<li>Item two</li>
+				{app.options.map((option) => (
+					<li key={option}>{option}</li>
+				))}
 			</ol>
 			<form onSubmit={onFormSubmit}>
 				<input type='text' name='option' />
