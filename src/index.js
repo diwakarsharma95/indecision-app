182719cac6530dd8ef87526e081744ac376f7803
Diwakar Sharma <diwakarsharma95@gmail.com>
2020-05-25 16:42:15 +1000

    ES6 Classes: Part I

--- src/index.js
+++ src/index.js
@@ -5,88 +5,26 @@ import * as serviceWorker from './serviceWorker';
 
 //************************************************************ */
 
-// const app = {
-// 	title: 'Indecision App',
-// 	subtitle: 'Put your life in the hands of computer',
-// 	options: [],
-// };
+class Person {
+	constructor(name = 'Anonymous', age = 0) {
+		this.name = name;
+		this.age = age;
+	}
+	getGreeting() {
+		return `Hi, I am ${this.name}!`;
+	}
+	getDescription() {
+		return `${this.name} is ${this.age} year(s) old`;
+	}
+}
 
-// const onFormSubmit = (e) => {
-// 	e.preventDefault();
-// 	const option = e.target.elements.option.value;
-// 	if (option) {
-// 		app.options.push(option);
-// 		e.target.elements.option.value = '';
-// 		render();
-// 	}
-// };
-// const onRemoveAll = () => {
-// 	app.options = [];
-// 	render();
-// };
+const me = new Person('Diwakar Sharma', 22);
+console.log(me.getGreeting());
+console.log(me.getDescription());
 
-// const onMakeDecision = () => {
-// 	const randomNum = Math.floor(Math.random() * app.options.length);
-// 	const option = app.options[randomNum];
-// 	alert(option);
-// };
-
-// const appRoot = document.getElementById('root');
-
-// const render = () => {
-// 	const template = (
-// 		<div>
-// 			<h1>{app.title}</h1>
-// 			{app.subtitle && <p>{app.subtitle}</p>}
-// 			<p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-// 			<button disabled={app.options.length === 0} onClick={onMakeDecision}>
-// 				What should I do ?
-// 			</button>
-// 			<button onClick={onRemoveAll}>Remove All</button>
-
-// 			<ol>
-// 				{app.options.map((option) => (
-// 					<li key={option}>{option}</li>
-// 				))}
-// 			</ol>
-// 			<form onSubmit={onFormSubmit}>
-// 				<input type='text' name='option' />
-// 				<button>Add Option</button>
-// 			</form>
-// 		</div>
-// 	);
-
-// 	ReactDOM.render(template, appRoot);
-// };
-
-// render();
-
-//*************************Challenge***************************/
-
-const btnValue = {
-	show: 'Show details',
-	hide: 'Hide details',
-	status: true,
-};
-
-const btnClicked = () => {
-	btnValue.status = !btnValue.status;
-	render();
-};
-
-const appRoot = document.getElementById('root');
-
-const render = () => {
-	const template = (
-		<div>
-			<h1>Visibility Toggle</h1>
-			<button onClick={btnClicked}>{btnValue.status === true ? btnValue.show : btnValue.hide}</button>
-			{btnValue.status === true ? null : <p>Hey. These are some details you can see now!</p>}
-		</div>
-	);
-	ReactDOM.render(template, appRoot);
-};
-render();
+const other = new Person();
+console.log(other.getGreeting());
+console.log(other.getDescription());
 
 //********************************************** */
 
