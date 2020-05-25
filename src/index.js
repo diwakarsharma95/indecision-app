febd01557ca5d346347dc78e2d353f72ef2cb1cc
Diwakar Sharma <diwakarsharma95@gmail.com>
2020-05-25 18:00:44 +1000

    ES6 Classes: Part II

--- src/index.js
+++ src/index.js
@@ -14,18 +14,54 @@ class Person {
 		return `Hi, I am ${this.name}!`;
 	}
 	getDescription() {
-		return `${this.name} is ${this.age} year(s) old`;
+		return `${this.name} is ${this.age} year(s) old.`;
 	}
 }
 
-const me = new Person('Diwakar Sharma', 22);
+class Student extends Person {
+	constructor(name, age, major) {
+		super(name, age);
+		this.major = major;
+	}
+	hasMajor() {
+		return !!this.major;
+	}
+	getDescription() {
+		let description = super.getDescription();
+
+		if (this.hasMajor()) {
+			description = description + ` Their major is ${this.major}`;
+		}
+
+		return description;
+	}
+}
+
+class Traveler extends Person {
+	constructor(name, age, homeLocation) {
+		super(name, age);
+		this.homeLocation = homeLocation;
+	}
+	getGreeting() {
+		let greeting = super.getGreeting();
+		if (this.homeLocation) {
+			greeting = greeting + ` I am visiting from ${this.homeLocation}`;
+		}
+		return greeting;
+	}
+}
+
+// const me = new Student('Diwakar Sharma', 22, 'Computer Science');
+// console.log(me.getDescription());
+
+// const other = new Student();
+// console.log(other.getDescription());
+
+const me = new Traveler('Diwakar Sharma', 22, 'Brisbane');
 console.log(me.getGreeting());
-console.log(me.getDescription());
 
-const other = new Person();
+const other = new Traveler(undefined, undefined, 'Nowhere');
 console.log(other.getGreeting());
-console.log(other.getDescription());
-
 //********************************************** */
 
 // ReactDOM.render(
