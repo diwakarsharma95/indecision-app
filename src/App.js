1348b60b82b4a2c464e3ddb023f3ab72c3d967db
Diwakar Sharma <diwakarsharma95@gmail.com>
2020-05-26 17:26:52 +1000

    Adding State to Counter App: Part I

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './Header';
 import Action from './Action';
 import Options from './Options';
 import AddOption from './AddOption';
 
-function App() {
-	const title = 'Indecision';
-	const subtitle = 'Put your life in the hands of a computer.';
-	const options = ['Thing one', 'Thing two', 'Thing three'];
-	return (
-		<div>
-			<Header title={title} subtitle={subtitle}></Header>
-			<Action></Action>
-			<Options options={options}></Options>
-			<AddOption></AddOption>
-		</div>
-	);
+//************************************************************/
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.handleOne = this.handleOne.bind(this);
+		this.handleMinusOne = this.handleMinusOne.bind(this);
+		this.handleReset = this.handleReset.bind(this);
+	}
+	handleOne() {
+		console.log('handleOne');
+	}
+	handleMinusOne() {
+		console.log('handleMinusOne');
+	}
+	handleReset() {
+		console.log('handleReset');
+	}
+	render() {
+		return (
+			<div>
+				<h1>Count: </h1>
+				<button onClick={this.handleOne}>+1</button>
+				<button onClick={this.handleMinusOne}>-1</button>
+				<button onClick={this.handleReset}>reset</button>
+			</div>
+		);
+	}
 }
+
+//************************************************************/
+
+// function App() {
+// 	const title = 'Indecision';
+// 	const subtitle = 'Put your life in the hands of a computer.';
+// 	const options = ['Thing one', 'Thing two', 'Thing three'];
+// 	return (
+// 		<div>
+// 			<Header title={title} subtitle={subtitle}></Header>
+// 			<Action></Action>
+// 			<Options options={options}></Options>
+// 			<AddOption></AddOption>
+// 		</div>
+// 	);
+// }
 
 export default App;
