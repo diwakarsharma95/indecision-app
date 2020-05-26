b5edc0151ef0272bc0a0c9891a159b433d5c1509
Diwakar Sharma <diwakarsharma95@gmail.com>
2020-05-26 14:19:44 +1000

    Method Binding

--- src/Options.js
+++ src/Options.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 import Option from './Option';
 
 class Options extends Component {
+	constructor(props) {
+		super(props);
+		this.handleRemoveAll = this.handleRemoveAll.bind(this);
+	}
 	handleRemoveAll() {
-		alert('All removed');
+		// alert('All removed');
+		console.log(this.props.options);
 	}
 	render() {
 		return (
