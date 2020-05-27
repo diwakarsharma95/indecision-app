a51ec38c3b4656c7377a36c9cd03aa00e9340eb3
Diwakar Sharma <diwakarsharma95@gmail.com>
2020-05-27 14:41:40 +1000

    Build It: Adding State to VisibilityToggle

--- src/App.js
+++ src/App.js
@@ -6,49 +6,81 @@ import AddOption from './AddOption';
 
 //************************************************************/
 
-class App extends Component {
+// class App extends Component {
+// 	constructor(props) {
+// 		super(props);
+// 		this.handleOne = this.handleOne.bind(this);
+// 		this.handleMinusOne = this.handleMinusOne.bind(this);
+// 		this.handleReset = this.handleReset.bind(this);
+// 		this.state = {
+// 			count: 0,
+// 		};
+// 	}
+// 	handleOne() {
+// 		this.setState((prevState) => {
+// 			return {
+// 				count: prevState.count + 1,
+// 			};
+// 		});
+// 	}
+// 	handleMinusOne() {
+// 		this.setState((prevState) => {
+// 			return {
+// 				count: prevState.count - 1,
+// 			};
+// 		});
+// 	}
+// 	handleReset() {
+// 		this.setState(() => {
+// 			return {
+// 				count: 0,
+// 			};
+// 		});
+// 	}
+// 	render() {
+// 		return (
+// 			<div>
+// 				<h1>Count: {this.state.count}</h1>
+// 				<button onClick={this.handleOne}>+1</button>
+// 				<button onClick={this.handleMinusOne}>-1</button>
+// 				<button onClick={this.handleReset}>reset</button>
+// 			</div>
+// 		);
+// 	}
+// }
+
+//*************************************************************/
+class Toggle extends Component {
 	constructor(props) {
 		super(props);
-		this.handleOne = this.handleOne.bind(this);
-		this.handleMinusOne = this.handleMinusOne.bind(this);
-		this.handleReset = this.handleReset.bind(this);
+		this.btnClicked = this.btnClicked.bind(this);
 		this.state = {
-			count: 0,
+			show: 'Show details',
+			hide: 'Hide details',
+			status: true,
 		};
 	}
-	handleOne() {
+
+	btnClicked() {
 		this.setState((prevState) => {
 			return {
-				count: prevState.count + 1,
-			};
-		});
-	}
-	handleMinusOne() {
-		this.setState((prevState) => {
-			return {
-				count: prevState.count - 1,
-			};
-		});
-	}
-	handleReset() {
-		this.setState(() => {
-			return {
-				count: 0,
+				status: !prevState.status,
 			};
 		});
 	}
 	render() {
 		return (
 			<div>
-				<h1>Count: {this.state.count}</h1>
-				<button onClick={this.handleOne}>+1</button>
-				<button onClick={this.handleMinusOne}>-1</button>
-				<button onClick={this.handleReset}>reset</button>
+				<h1>Visibility Toggle</h1>
+				<button onClick={this.btnClicked}>
+					{this.state.status === true ? this.state.show : this.state.hide}
+				</button>
+				{this.state.status === true ? null : <p>Hey. These are some details you can see now!</p>}
 			</div>
 		);
 	}
 }
-
+export default Toggle;
 //************************************************************/
 
 // function App() {
@@ -65,4 +97,4 @@ class App extends Component {
 // 	);
 // }
 
-export default App;
+// export default App;
