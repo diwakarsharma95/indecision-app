3cd791cc426eceec63969cf06e47a305b6e700e8
Diwakar Sharma <diwakarsharma95@gmail.com>
2020-05-27 17:35:40 +1000

    Indecision State: Part II

--- src/AddOption.js
+++ src/AddOption.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 
 class AddOption extends Component {
+	constructor(props) {
+		super(props);
+		this.handleAddOption = this.handleAddOption.bind(this);
+		this.state = {
+			error: undefined,
+		};
+	}
 	handleAddOption(e) {
 		e.preventDefault();
-		const inputOption = e.target.elements.option.value;
-		if (inputOption.trim()) {
-			alert(inputOption);
-			e.target.elements.option.value = '';
-		}
+		const inputOption = e.target.elements.option.value.trim();
+		const error = this.props.handleAddOption(inputOption);
+		e.target.elements.option.value = '';
+		this.setState(() => {
+			return { error };
+		});
 	}
 	render() {
 		return (
 			<div>
+				{this.state.error && <p>{this.state.error}</p>}
 				<form onSubmit={this.handleAddOption}>
 					<input type='text' name='option'></input>
 					<button>Add Option</button>
--- src/App.js
+++ src/App.js
@@ -9,10 +9,11 @@ class App extends Component {
 		super(props);
 		this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
 		this.handlePick = this.handlePick.bind(this);
+		this.handleAddOption = this.handleAddOption.bind(this);
 		this.state = {
 			title: 'Indecision',
 			subtitle: 'Put your life in the hands of a computer.',
-			options: ['Thing one', 'Thing two', 'Thing three'],
+			options: [],
 		};
 	}
 	handleDeleteOptions() {
@@ -27,13 +28,25 @@ class App extends Component {
 		const option = this.state.options[randomNum];
 		alert(option);
 	}
+	handleAddOption(option) {
+		if (!option) {
+			return 'Enter valid value to add item';
+		} else if (this.state.options.indexOf(option) > -1) {
+			return 'This option already exists';
+		}
+		this.setState((prevState) => {
+			return {
+				options: prevState.options.concat(option),
+			};
+		});
+	}
 	render() {
 		return (
 			<div>
 				<Header title={this.state.title} subtitle={this.state.subtitle}></Header>
 				<Action hasOptions={this.state.options.length > 0} handlePick={this.handlePick}></Action>
 				<Options options={this.state.options} handleDeleteOptions={this.handleDeleteOptions}></Options>
-				<AddOption></AddOption>
+				<AddOption handleAddOption={this.handleAddOption}></AddOption>
 			</div>
 		);
 	}
