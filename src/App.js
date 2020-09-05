7a7a9c4cc2bf93b1efcf5a0e1c97ba5fec17e629
Diwakar Sharma <diwakarsharma95@gmail.com>
2020-09-05 12:13:25 +1000

    dependencies added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
+import './styles/styles.scss';
 
-ReactDOM.render(<IndecisionApp options={['test']} />, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
